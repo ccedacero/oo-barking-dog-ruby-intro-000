dc73eeeee29a6095cd73a0b996f7b6cf46c1013c
Cristian Cedacero <cristian.cedacero@gmail.com>
2020-03-22 01:57:07 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,6 @@
 class Dog
-  def initialize(name)
-    @name = name
+  def name=(dog_name)
+    @name= dog_name
   end
   
   def name 
@@ -10,5 +10,4 @@ class Dog
   def bark
     @bark = 'woof!'
   end
-  
 end
